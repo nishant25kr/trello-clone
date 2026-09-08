4ef9d449ddb05be04bc2aef0cba09ce394f64fc1
Nishant kumar <nishant927472@gmail.com>
2026-09-08 22:49:53 +0530

    feat: implement WebSocket connection for issue management and join board functionality

--- apps/frontend/src/Components/Issues.tsx
+++ apps/frontend/src/Components/Issues.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export const Issue = () => {
     const params = useParams()
-    console.log(params)
+    let ws = useRef<WebSocket>(null)
+    const wsRef = useRef<WebSocket | null>(null);
+    useEffect(()=>{
+        const ws = new WebSocket('ws://localhost:8080');
+        wsRef.current = ws;
+        
+        ws.onopen = () => {
+            ws.send(JSON.stringify({
+                type:'join',
+                payload:{
+                    boardId:params
+                }
+            }))
+        }
+    },[])
 
     return (
         <>
